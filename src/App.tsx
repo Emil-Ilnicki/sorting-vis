aeb48539498dcd7bc3f7fa75f357d6e0d814a987
Emil-Ilnicki <emililnicki@gmail.com>
2021-09-13 23:41:41 -0400

    Added basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div>The start of something great</div>;
+  const [array, setArray] = useState<number[]>([0, 0, 0, 0]);
+
+  useEffect(() => {
+    setArray([1, 2, 3, 4]);
+  }, []);
+
+  return (
+    <div className="array-container">
+      {array.map((value) => (
+        <div className="array-value">{value}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;

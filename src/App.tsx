1835c20c4eafa9ccc73f389152e423fe96c06da9
Emil-Ilnicki <emililnicki@gmail.com>
2021-09-14 23:24:02 -0400

    added css to bars to divs, still need to incorporate a drop down menu to choose which algorithm the user wants

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,42 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [array, setArray] = useState<number[]>([0, 0, 0, 0]);
+  const [array, setArray] = useState<number[]>([]);
 
   useEffect(() => {
-    setArray([1, 2, 3, 4]);
+    setArray([4, 2, 1, 3, 1, 1, 1, 2, 3, 1]);
   }, []);
 
+  const swapArray = () => {
+    setArray(() => {
+      let newArray: number[] = [...array];
+
+      let temp: number = newArray[0];
+      newArray[0] = newArray[newArray.length - 1];
+      newArray[newArray.length - 1] = temp;
+
+      return newArray;
+    });
+  };
+
   return (
-    <div className="array-container">
-      {array.map((value) => (
-        <div className="array-value">{value}</div>
-      ))}
+    <div className="app-container">
+      <div className="array-container">
+        {array.map((value) => (
+          <div
+            style={{
+              backgroundColor: "#888",
+              height: `${value + 0.5}em`,
+              width: "1em",
+              borderRadius: "2em",
+              marginTop: "auto",
+            }}
+          ></div>
+        ))}
+      </div>
+      <div className="button-container">
+        <button onClick={swapArray}> Swap Array </button>
+      </div>
     </div>
   );
 }
